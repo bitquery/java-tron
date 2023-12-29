b0e08de9b926f35739e5efc14e6106ef2e1395a9
Ruslan <kros8925@gmail.com>
2023-12-29 16:57:42 +0200

    refactor: getMeta() instread of this.getMeta()

--- streaming/src/main/java/org/tron/streaming/messages/ProtobufMessage.java
+++ streaming/src/main/java/org/tron/streaming/messages/ProtobufMessage.java
@@ -20,8 +20,8 @@ public class ProtobufMessage {
     private byte[] body;
 
     public ProtobufMessage(BlockMessageDescriptor descriptor, byte[] body) {
-        this.getMeta().setDescriptor(descriptor);
-        this.getMeta().setSize(body.length);
+        getMeta().setDescriptor(descriptor);
+        getMeta().setSize(body.length);
 
         this.body = body;
     }
@@ -29,21 +29,21 @@ public class ProtobufMessage {
     public void storeMessage(String streamingDirectory) {
         String directoryPath = getDirectoryName(streamingDirectory);
         String fileName = String.format("%d_%s_%s%s",
-                this.getMeta().getDescriptor().getBlockNumber(),
-                this.getMeta().getDescriptor().getBlockHash(),
+                getMeta().getDescriptor().getBlockNumber(),
+                getMeta().getDescriptor().getBlockHash(),
                 getBodyHash(),
                 ".lz4"
         );
 
         String fullPath = Paths.get(directoryPath, fileName).toString();
 
-        this.getMeta().setUri(fullPath);
+        getMeta().setUri(fullPath);
 
         writeMessageToFileWithCompression(fullPath);
     }
 
     private String getDirectoryName(String streamingDirectory) {
-        String blockFolder =  String.valueOf(1000 * (this.getMeta().getDescriptor().getBlockNumber() / 1000));
+        String blockFolder =  String.valueOf(1000 * (getMeta().getDescriptor().getBlockNumber() / 1000));
         String directoryPath = Paths.get(streamingDirectory, blockFolder).toString();
 
         return directoryPath;
