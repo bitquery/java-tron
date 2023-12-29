ec547ba1bff961c1a44dd97cbda9829d3d7dc596
Ruslan <kros8925@gmail.com>
2023-12-29 16:32:12 +0200

    refactor: @data instread of @getter and @setter

--- streaming/src/main/java/org/tron/streaming/BlockMessageDescriptor.java
+++ streaming/src/main/java/org/tron/streaming/BlockMessageDescriptor.java
@@ -1,22 +1,11 @@
 package org.tron.streaming;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 public class BlockMessageDescriptor {
-  @Getter
-  @Setter
   private String blockHash;
-
-  @Getter
-  @Setter
   private long blockNumber;
-
-  @Getter
-  @Setter
   private String parentHash;
-
-  @Getter
-  @Setter
   private long parentNumber;
 }
